4c2b7eed86f7903895b2e52272f4f5aa6dfae43b
Mukesh <mpurohit88@gmail.com>
2019-04-05 15:44:50 +0530

    quote product changes

--- server/models/quoteProduct.js
+++ server/models/quoteProduct.js
@@ -1,32 +1,32 @@
 const connection = require("../lib/connection.js");
-const QuoteProduct = function(){
-  
+const QuoteProduct = function () {
+
 };
 
 
-QuoteProduct.prototype.getByQuoteId = function(quoteId) {
-  return new Promise(function(resolve, reject) {
-    connection.getConnection(function(error, connection){
-      console.log('Process Started %d All',connection.threadId);
+QuoteProduct.prototype.getByQuoteId = function (quoteId) {
+	return new Promise(function (resolve, reject) {
+		connection.getConnection(function (error, connection) {
+			console.log('Process Started %d All', connection.threadId);
 
-      if (error) {
-        throw error;
+			if (error) {
+				throw error;
 			}
-			
-			connection.query('select qp.product_id, p.name, qp.quantity, qp.gstn, qp.rate, p.hsnCode, p.imgName, p.description from quote_product qp inner join product p on qp.product_id = p.id where qp.quote_id = ?', [quoteId], function(error,rows,fields){
-					
-				if(!error){ 
-						resolve(rows);
+
+			connection.query('select qp.product_id, p.name, p.unit, qp.quantity, qp.gstn, qp.rate, p.hsnCode, p.imgName, p.description from quote_product qp inner join product p on qp.product_id = p.id where qp.quote_id = ?', [quoteId], function (error, rows, fields) {
+
+				if (!error) {
+					resolve(rows);
 				} else {
-						console.log("Error...", error);
-						reject(error);
+					console.log("Error...", error);
+					reject(error);
 				}
 
 				connection.release();
-				console.log('Process Complete %d',connection.threadId);
+				console.log('Process Complete %d', connection.threadId);
 			});
-    });
-  });
+		});
+	});
 }
 
 module.exports = QuoteProduct;
