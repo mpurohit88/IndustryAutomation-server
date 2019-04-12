e872e2f795752ce9023b4f356b03b351160d5019
Mukesh <mpurohit88@gmail.com>
2019-04-12 10:59:21 +0530

    user id and password changes

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,50 +1,51 @@
 const User = require("../models/user.js")
 
-const register = function(req, res, next){
-    let params = {
-					createdBy: req.decoded.id,
-					organizationId: req.decoded.organizationId,
-					companyId: req.body.company_name,
-					name:req.body.name,
-					password:req.body.password,
-					designation: req.body.designation,
-					area: req.body.area,
-					address: req.body.address,
-					mobNo: req.body.mobNo,
-					role: 'user',
-					isActive: 1,
-					email: req.body.email
-			};
-    const newUser = new User(params);
+const register = function (req, res, next) {
+	let params = {
+		createdBy: req.decoded.id,
+		organizationId: req.decoded.organizationId,
+		companyId: req.body.company_name,
+		name: req.body.name,
+		userId: req.body.userId,
+		password: req.body.password,
+		designation: req.body.designation,
+		area: req.body.area,
+		address: req.body.address,
+		mobNo: req.body.mobNo,
+		role: 'user',
+		isActive: 1,
+		email: req.body.email
+	};
+	const newUser = new User(params);
 
-    try {
-       newUser.register().then(function(result) {
-		if(req.decoded.role === 'admin') {
-			new User({}).all().then(function(userList) {
-				res.send({credentials: result, userList: userList});
-			});
-		} else {
-			res.send([]);
-		}
-	   });
-    } catch (err) {
-			console.log("Error: ", err);
-    }
+	try {
+		newUser.register().then(function (result) {
+			if (req.decoded.role === 'admin') {
+				new User({}).all().then(function (userList) {
+					res.send({ credentials: result, userList: userList });
+				});
+			} else {
+				res.send([]);
+			}
+		});
+	} catch (err) {
+		console.log("Error: ", err);
+	}
 };
 
 
-const all = function(req, res, next){
+const all = function (req, res, next) {
 	try {
-        if(req.decoded.role === 'admin') {
-			new User({}).all().then(function(userList) {
-				res.send({userList: userList});
+		if (req.decoded.role === 'admin') {
+			new User({}).all().then(function (userList) {
+				res.send({ userList: userList });
 			});
 		} else {
 			res.send([]);
 		}
- 	} catch (err) {
-	 	console.log("Error: ", err);
- 	}
+	} catch (err) {
+		console.log("Error: ", err);
+	}
 }
 
-module.exports = {register: register, all: all};
+module.exports = { register: register, all: all };
--- server/models/user.js
+++ server/models/user.js
@@ -1,42 +1,43 @@
 const connection = require("../lib/connection.js");
 const utils = require("../utils");
 
-var User = function(params){
-   this.email = params.email;
-   this.password = utils.randomString(11);
-   this.companyId = params.companyId;
-   this.organizationId = params.organizationId;
-   this.name = params.name;
-   this.designation = params.designation;
-   this.area = params.area;
-   this.address = params.address;
-   this.mobNo = params.mobNo;
-   this.role = params.role;
-   this.isActive = params.isActive;
-   this.createdBy = params.createdBy;
+var User = function (params) {
+  this.email = params.email;
+  this.password = params.password; //utils.randomString(11);
+  this.companyId = params.companyId;
+  this.organizationId = params.organizationId;
+  this.name = params.name;
+  this.userId = params.userId;
+  this.designation = params.designation;
+  this.area = params.area;
+  this.address = params.address;
+  this.mobNo = params.mobNo;
+  this.role = params.role;
+  this.isActive = params.isActive;
+  this.createdBy = params.createdBy;
 };
 
-User.prototype.register = function(newUser){
+User.prototype.register = function (newUser) {
   const that = this;
-  return new Promise(function(resolve, reject) {
-  connection.getConnection(function(error, connection){
-    if (error) {
-			throw error;
-    }
-    
-    // let values = [
-    //   [that.company_id, that.name, AES_ENCRYPT(that.password, 'secret'), that.designation, that.address, that.area, that.mobNo, that.email, that.isActive, that.createdBy]
-    // ]
-    
+  return new Promise(function (resolve, reject) {
+    connection.getConnection(function (error, connection) {
+      if (error) {
+        throw error;
+      }
 
-    connection.query('SELECT MAX(id) as id FROM user', function(error,rows,fields){
-      const userId = that.name.slice(0, 4).toLowerCase() + (rows[0].id + 1);
-        
-        if(!error){ 
-          connection.query('INSERT INTO user(companyId,organizationId,name,userId,password,designation,address,area,mobileNo,email,role,isActive,createdBy) VALUES ("' + that.companyId + '", "' + that.organizationId + '", "' + that.name + '", "' + userId + '", AES_ENCRYPT("' + that.password + '", "secret"), "' + that.designation + '", "' + that.area + '", "' + that.address + '", "' + that.mobNo + '", "' + that.email + '", "' + that.role + '", "' + that.isActive + '", "' + that.createdBy + '")', function(error,rows,fields){
-      
-            if(!error){ 
-              resolve({userName: that.name, userId: userId, password: that.password});
+      // let values = [
+      //   [that.company_id, that.name, AES_ENCRYPT(that.password, 'secret'), that.designation, that.address, that.area, that.mobNo, that.email, that.isActive, that.createdBy]
+      // ]
+
+
+      connection.query('SELECT MAX(id) as id FROM user', function (error, rows, fields) {
+        // const userId = that.name.slice(0, 4).toLowerCase() + (rows[0].id + 1);
+
+        if (!error) {
+          connection.query('INSERT INTO user(companyId,organizationId,name,userId,password,designation,address,area,mobileNo,email,role,isActive,createdBy) VALUES ("' + that.companyId + '", "' + that.organizationId + '", "' + that.name + '", "' + that.userId + '", AES_ENCRYPT("' + that.password + '", "secret"), "' + that.designation + '", "' + that.area + '", "' + that.address + '", "' + that.mobNo + '", "' + that.email + '", "' + that.role + '", "' + that.isActive + '", "' + that.createdBy + '")', function (error, rows, fields) {
+
+            if (!error) {
+              resolve({ userName: that.name, userId: that.userId, password: that.password });
             } else {
               console.log("Error...", error);
               reject(error)
@@ -48,16 +49,16 @@ User.prototype.register = function(newUser){
         }
 
         connection.release();
-        console.log('Process Complete %d',connection.threadId);
+        console.log('Process Complete %d', connection.threadId);
       });
     });
   });
 };
 
-User.prototype.all = function() {
-  return new Promise(function(resolve, reject) {
-    connection.getConnection(function(error, connection){
-      console.log('Process Started %d All',connection.threadId);
+User.prototype.all = function () {
+  return new Promise(function (resolve, reject) {
+    connection.getConnection(function (error, connection) {
+      console.log('Process Started %d All', connection.threadId);
 
       if (error) {
         throw error;
@@ -65,18 +66,18 @@ User.prototype.all = function() {
 
       const isActive = 1;
 
-			connection.query('select u.id, c.name as companyName, u.name, u.userId, u.designation, u.area, u.address, u.mobileNo, u.email, u.dateTimeCreated from user u inner join company c on u.companyId = c.id where u.isActive=?', [isActive], function(error,rows,fields){
-			 
-					if(!error){ 
-						resolve(rows);
-					} else {
-						console.log("Error...", error);
-						reject(error);
-					}
+      connection.query('select u.id, c.name as companyName, u.name, u.userId, u.designation, u.area, u.address, u.mobileNo, u.email, u.dateTimeCreated from user u inner join company c on u.companyId = c.id where u.isActive=?', [isActive], function (error, rows, fields) {
 
-					connection.release();
-					console.log('Process Complete %d',connection.threadId);
-				});
+        if (!error) {
+          resolve(rows);
+        } else {
+          console.log("Error...", error);
+          reject(error);
+        }
+
+        connection.release();
+        console.log('Process Complete %d', connection.threadId);
+      });
     });
   });
 }
